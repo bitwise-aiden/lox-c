c22ff543ceee8292dd095f253d66a3efe85d5332
Aiden Storey <git@storey.dev>
2023-08-31 22:39:27 -0400

    Chunks of Bytecode
    Following chapter 14: https://craftinginterpreters.com/chunks-of-bytecode.html

---
+++ src/debug.h
@@ -0,0 +1,9 @@
+#ifndef lox_c_debug_h
+#define lox_c_debug_h
+
+#include "chunk.h"
+
+void disassembleChunk(Chunk* chunk, const char* name);
+int disassembleInstruction(Chunk* chunk, int offset);
+
+#endif
---
+++ src/value.h
@@ -0,0 +1,8 @@
+#ifndef lox_c_value_h
+#define lox_c_value_h
+
+#include "common.h"
+
+typedef double Value;
+
+#endif
